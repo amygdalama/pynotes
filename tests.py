53ecadc15475a21578343d5798df73381d42e00e
Amy Hanlon <amyehanlon@gmail.com>
2014-07-11 14:51:28 -0400

    add delete note test

--- tests.py
+++ tests.py
@@ -18,12 +18,24 @@ class AddTest(unittest.TestCase):
     def tearDown(self):
         os.remove('notes/%s.txt' % self.title)
 
-    def test_added_note_exists(self):
+    def test_note_exists(self):
         self.assertTrue(os.path.isfile('notes/%s.txt' % self.title))
 
     def test_note_content(self):
         saved_content = open('notes/%s.txt' % self.title).read()
         self.assertEqual(saved_content, self.content)
+
+
+class DeleteTest(unittest.TestCase):
+
+    def setUp(self):
+        self.title = 'test'
+        self.content = 'whatever'
+        pynotes.add_note(self.title, self.content)
+        pynotes.delete_note(self.title)
+
+    def test_note_doesnt_exist(self):
+        self.assertFalse(os.path.exists('notes/%s.txt' % self.title))
 
 
 if __name__ == '__main__':
